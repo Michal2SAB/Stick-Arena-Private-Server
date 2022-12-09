f2a56c1eff24a20e28795dc0bd74a2639df5005d
Michał <38110165+Michal2SAB@users.noreply.github.com>
2022-12-09 11:56:58 +0100

    Create index.php

---
+++ htdocs/stickarena/index.php
@@ -0,0 +1,15 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+?>
+<html>
+<body>
+<div style="align:center">
+<object width="550" height="400">
+<param name="movie" value="game.swf">
+<embed src="game.swf" width="550" height="400">
+</embed>
+</object>
+</div>
+</body>
+</html>
